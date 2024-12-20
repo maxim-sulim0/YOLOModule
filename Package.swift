b1e1d3b43899cbee5c1cc42ce12ab18e53bd758d
maximsulim <cool.sulim@list.ru>
2024-12-20 19:06:58 +0300

    update package

--- Package.swift
+++ Package.swift
@@ -1,21 +1,24 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "YOLOModule",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "YOLOModule",
             targets: ["YOLOModule"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "YOLOModule"),
+            name: "YOLOModule",
+            dependencies: [],
+            resources: [
+                .copy("Yolov3.mlpackage")
+            ]),
         .testTarget(
             name: "YOLOModuleTests",
             dependencies: ["YOLOModule"]),
